0f90fb738ca038f4eac5afcef2355cb2ade10fa5
Yussef Rodriguez <yussef.rodriguez1995@gmail.com>
2024-11-15 10:07:57 -0600

    desarrollo principal

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas  as pd
+import plotly.express as px
+
+
+car_data = pd.read_csv('C:\\Users\\yrodriguezc\\Documents\\yusa\\herramientasSoftwareProyect\\vehicles_us.csv')
+
+st.header('Anuncios de venta de coches')
+
+hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir grafico de dispercion') # crear un botón
+
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+     # escribir un mensaje
+    st.write('Creación de un grafico de dispercion para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig1 = px.scatter(car_data, x="sepal_width", y="sepal_length")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig1, use_container_width=True)
